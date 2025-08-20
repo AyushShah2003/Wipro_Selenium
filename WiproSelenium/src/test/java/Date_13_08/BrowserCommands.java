00c03997e2dbfd65e9dff8627648f79127dee008
Ayush Shah <95910417+AyushShah2003@users.noreply.github.com>
2025-08-20 23:57:20 +0530

    Commit

---
+++ WiproSelenium/src/test/java/Date_13_08/BrowserCommands.java
@@ -0,0 +1,23 @@
+package Date_13_08;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserCommands {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("https://rahulshettyacademy.com/AutomationPractice/");
+		String title=driver.getTitle();
+		System.out.print(title);
+		Thread.sleep(2000);
+String pageSource=driver.getPageSource();
+System.out.print(pageSource);
+		Thread.sleep(2000);
+String currentUrl=driver.getCurrentUrl();
+System.out.print(currentUrl);
+
+	}
+
+}
